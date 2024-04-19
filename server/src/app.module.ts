78a9a4a7353f452f68de492f4798d56e987259c6
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2024-04-18 21:06:18 -0300

    chore: configuração do TypeORM

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

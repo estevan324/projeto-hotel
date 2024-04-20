0503aa1ab932b9b7424956b5e53129d00de1da16
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2024-04-20 17:54:39 -0300

    chore: habilitado cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,9 @@ import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (validationErrors: ValidationError[] = []) => {

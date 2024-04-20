c6d0dee9311b1df53d871f79af69928648d3fea6
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2024-04-20 17:15:02 -0300

    fix: movido arquivos de configurações

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { navLinks } from "../configs/constants";
+import { navLinks } from "@/configs/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 

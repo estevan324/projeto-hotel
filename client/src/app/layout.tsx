8b5b628352eee99c7148c2bc98613fb944c9109e
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2024-04-20 15:57:04 -0300

    style: configuração dos estilos do bootstrap e do bootstrap bundle

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Montserrat } from "next/font/google";
+import "@/styles/globals.scss";
+import BootstrapBundle from "@/components/configs/BootstrapBundle";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <BootstrapBundle />
+      <body className={montserrat.className}>{children}</body>
     </html>
   );
 }

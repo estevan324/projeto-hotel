2b63a8d3d57e50a86bc591d69c5d2bd78dd1e9d3
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2024-04-23 21:14:55 -0300

    chore: validação para criação de reservas

--- server/src/reservation/reservation.service.ts
+++ server/src/reservation/reservation.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ReservationEntity } from './entities/reservation.entity';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
 import { CreateReservationDTO } from './dtos/create-reservation.dto';
 import { UpdateReservationDTO } from './dtos/update-reservation.dto';
 import { PaginationService } from 'src/pagination/pagination.service';
@@ -25,6 +29,37 @@ export class ReservationService {
     return reservation;
   }
 
+  private async checkOutIsAfterCheckIn(checkIn: Date, checkOut: Date) {
+    if (checkOut <= checkIn) {
+      throw new BadRequestException(
+        'Check-out date must be after check-in date',
+      );
+    }
+
+    return true;
+  }
+
+  private async hasOverlappingDates(
+    roomId: number,
+    checkIn: Date,
+    checkOut: Date,
+  ) {
+    const reservation = await this.reservationRepository.findOne({
+      where: {
+        roomId,
+        checkIn: Between(checkIn, checkOut),
+      },
+    });
+
+    if (reservation) {
+      throw new BadRequestException(
+        'Reservation dates overlap with another reservation',
+      );
+    }
+
+    return true;
+  }
+
   async findAll(page?: number, limit?: number) {
     return this.paginationService.paginate(
       this.reservationRepository,
@@ -38,12 +73,18 @@ export class ReservationService {
   }
 
   async create(data: CreateReservationDTO) {
+    await this.checkOutIsAfterCheckIn(data.checkIn, data.checkOut);
+    await this.hasOverlappingDates(data.roomId, data.checkIn, data.checkOut);
+
     const reservation = this.reservationRepository.create(data);
     return this.reservationRepository.save(reservation);
   }
 
   async update(id: number, data: UpdateReservationDTO) {
     const room = await this.exists(id);
+
+    await this.checkOutIsAfterCheckIn(data.checkIn, data.checkOut);
+    await this.hasOverlappingDates(room.room.id, data.checkIn, data.checkOut);
 
     this.reservationRepository.merge(room, data);
 
